a4bd56007f3be019261ed24fb93398c6de18663e
satyakinkohli <satyakinkohli@gmail.com>
2020-11-11 23:49:48 +0530

    logos added

--- Travel/models/airlines.py
+++ Travel/models/airlines.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Airline(models.Model):
     name = models.CharField(max_length=50)
+    logos = models.ImageField(upload_to = "uploads/airline_logos", null=True, default=None)
 
     def __str__(self):
         return self.name

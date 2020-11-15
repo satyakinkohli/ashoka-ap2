e9e294d2282ebb4e954a30ce655a3e54d63a547e
Mehul Gupta <mehu.gupta_ug22@ashoka.edu.in>
2020-11-15 21:15:12 +0530

    dictionary done for location

--- Travel/views/flights_view.py
+++ Travel/views/flights_view.py
@@ -7,17 +7,14 @@ from Travel.models.flights import Flight
 class Flight_View(View):
     def get(self, request):
         flight_from = request.GET.get('flight_from')
-        print(flight_from)
+        locations = {'Delhi':1 , 'Mumbai':2 , 'Bangalore':3 , 'Pune':4 , 'Kolkata':5 , 'Jaipur':6}
+        from_id=(locations[flight_from])
+
+        #print(flight_from)
         flight_destination = request.GET.get('flight_destination')
-        print(flight_destination)
+        #print(flight_destination)
 
-        if flight_from == 'Delhi':
-        	from_id = 1
-
-        if flight_destination == 'Mumbai':
-        	destination_id = 2
-
-        #possible_flights = Flight.get_correct_flight(flight_from, flight_destination)
+        destination_id=(locations[flight_destination])
         possible_flights = Flight.get_correct_flight(from_id,destination_id)
         print (possible_flights)
 
@@ -28,3 +25,5 @@ class Flight_View(View):
 
 # def Flight_View(request):
 #     return render(request, 'flight_list.html')
+
+

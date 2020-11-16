aba3fd6cbdf9d99a32ec9cdb323b250fea7b5270
satyakinkohli <satyakinkohli@gmail.com>
2020-11-16 19:09:57 +0530

    date filter is now working

--- Travel/urls.py
+++ Travel/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views.homepage import Homepage
+from .views.homepage_view import Homepage
 from .views.flights_view import Flight_View
 
 urlpatterns = [
--- Travel/views/flights_view.py
+++ Travel/views/flights_view.py
@@ -6,24 +6,19 @@ from Travel.models.flights import Flight
 
 class Flight_View(View):
     def get(self, request):
+
+        locations = {'Delhi': 1, 'Mumbai': 2, 'Bangalore': 3, 'Pune': 4, 'Kolkata': 5, 'Jaipur': 6}
+        
         flight_from = request.GET.get('flight_from')
-        locations = {'Delhi':1 , 'Mumbai':2 , 'Bangalore':3 , 'Pune':4 , 'Kolkata':5 , 'Jaipur':6}
-        from_id=(locations[flight_from])
-
-        #print(flight_from)
+        flight_from_id=(locations[flight_from])
+        
         flight_destination = request.GET.get('flight_destination')
-        #print(flight_destination)
-
-        destination_id=(locations[flight_destination])
-        possible_flights = Flight.get_correct_flight(from_id,destination_id)
-        print (possible_flights)
-
+        flight_destination_id = (locations[flight_destination])
+        
+        flight_date = request.GET.get('flight_date')
+        
+        possible_flights = Flight.get_correct_flight_through_location_and_date(flight_from_id, flight_destination_id, flight_date)
+        
         data = {'possible_flights': possible_flights}
         
         return render(request, 'flight_list.html', data)
-
-
-# def Flight_View(request):
-#     return render(request, 'flight_list.html')
-
-

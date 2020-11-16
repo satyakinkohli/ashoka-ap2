ca26816db36bbff4a6c24f70d41933370454a504
satyakinkohli <satyakinkohli@gmail.com>
2020-11-16 23:37:50 +0530

    hotels_list page added

--- Travel/urls.py
+++ Travel/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 
 from .views.homepage_view import Homepage
 from .views.flights_view import Flight_View
+from .views.hotels_view import Hotel_View
 
 urlpatterns = [
     path('', Homepage, name="homepage"),
     path('flights', Flight_View.as_view(), name="flights"),
+    path('hotels', Hotel_View.as_view(), name="hotels"),
 ]

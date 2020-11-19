fd81d84df86815d75678679ec696ee2eaf27af96
adititibarewal26 <aditi.tibarewal_ug22@ashoka.edu.in>
2020-11-19 19:01:24 +0530

    try

--- Travel/static/scripts/homepage_service_difference.js
+++ Travel/static/scripts/homepage_service_difference.js
@@ -9,12 +9,12 @@ document.getElementById("hotels_search_bar").style.display = "none";
 document.getElementById("cabs_search_bar").style.display = "none";
 
 window.onload = function hotel_outlook() {
-    document.body.style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/0/09/Mumbai_Aug_2018_%2843397784544%29.jpg')";
+    document.body.style.backgroundImage = "url('https://i.imgur.com/r58irru.jpeg')";
     document.getElementById("hotels_search_bar").style.display = "block";
 }
 
 function hotel_outlook() {
-    document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')";
+    document.body.style.backgroundImage = "url('https://i.imgur.com/r58irru.jpeg')";
     var x = document.getElementById("hotels_search_bar");
     document.getElementById("flights_search_bar").style.display = "none";
     document.getElementById("cabs_search_bar").style.display = "none";

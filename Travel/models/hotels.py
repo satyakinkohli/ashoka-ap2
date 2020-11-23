19aa6dd3bc8cc07c9329bf95b4cdc13032f3366e
Mehul Gupta <mehu.gupta_ug22@ashoka.edu.in>
2020-11-24 01:59:25 +0530

    Hotels done besides summary

--- Travel/models/hotels.py
+++ Travel/models/hotels.py
@@ -6,6 +6,8 @@ class Hotel(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=250, default="The most comfortable stay", null=True, blank=True)
     location = models.ForeignKey(Location, on_delete=models.CASCADE, default=1)
+    locality = models.CharField(max_length=50, default = "X")
+    street = models.CharField(max_length=50, default= "Y")
     number = models.PositiveSmallIntegerField(default=9999999900)
     website = models.URLField(default="www..com", max_length=50)
     rating = models.DecimalField(max_digits=3, decimal_places=2, default=0)

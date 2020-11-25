ea456b13d753ce0d5f56ced2e079005c41ac6645
satyakinkohli <satyakinkohli@gmail.com>
2020-11-25 19:51:31 +0530

    hotel receipt also working

--- Travel/views/hotel_summary_view.py
+++ Travel/views/hotel_summary_view.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from Travel.models.hotels import Hotel
 from Travel.models.hotel_booking import Hotel_booking
 
+from datetime import datetime, date
+
 
 class Hotel_Summary_View(View):
     def get(self, request):
@@ -13,11 +15,30 @@ class Hotel_Summary_View(View):
 
         hotel_check_in = request.GET.get('hotel_check_in')
         hotel_check_out = request.GET.get('hotel_check_out')
-        print(hotel_check_in)
-        print(hotel_check_out)
+
         hotel_check_in_1 = datetime.strptime(hotel_check_in, '%Y-%m-%d')
         hotel_check_out_1 = datetime.strptime(hotel_check_out, '%Y-%m-%d')
         days = hotel_check_out_1 - hotel_check_in_1
+
+        # converting date to required format
+        now = date(*map(int, hotel_check_in.split('-')))
+
+        hotel_min_time = datetime.min.time()
+        hotel_check_in_adjust = datetime.combine(now, hotel_min_time)
+
+        hotel_check_in_formatted = datetime.strftime(
+            hotel_check_in_adjust, "%A; %d %b. %Y")
+        # end conversion
+
+        # converting date to required format
+        now = date(*map(int, hotel_check_out.split('-')))
+
+        hotel_min_time = datetime.min.time()
+        hotel_check_out_adjust = datetime.combine(now, hotel_min_time)
+
+        hotel_check_out_formatted = datetime.strftime(
+            hotel_check_out_adjust, "%A; %d %b. %Y")
+        # end conversion
 
         standard_number = request.GET.get('standard')
         # print(standard_number)
@@ -28,6 +49,9 @@ class Hotel_Summary_View(View):
         suite_number = request.GET.get('suite')
         # print(suite_number)
 
+        number_of_rooms = int(standard_number) + int(deluxe_number) + \
+            int(premium_number) + int(suite_number)
+
         price_per_night = (int(standard_number)*float(hotel_instance.standard_price)) + (int(deluxe_number)*float(hotel_instance.deluxe_price)
                                                                                          ) + (int(premium_number)*float(hotel_instance.premium_price)) + (int(suite_number)*float(hotel_instance.suite_price))
         print(price_per_night)
@@ -35,20 +59,22 @@ class Hotel_Summary_View(View):
         print(total_price)
 
         user = request.user
-        hotel_booking_instance = Hotel_booking(user_email = user.email,
-                                                user_fname = user.first_name,
-                                                user_lname = user.last_name,
-                                                hotel = hotel_instance,
-                                                check_in_date = hotel_check_in_1,
-                                                check_out_date = hotel_check_out_1,
-                                                total_price = total_price,
-                                                standard_number = int(standard_number),
-                                                deluxe_number = int(deluxe_number),
-                                                premium_number = int(premium_number),
-                                                suite_number = int(suite_number))
+        hotel_booking_instance = Hotel_booking(user_email=user.email,
+                                               user_fname=user.first_name,
+                                               user_lname=user.last_name,
+                                               hotel=hotel_instance,
+                                               check_in_date=hotel_check_in_1,
+                                               check_out_date=hotel_check_out_1,
+                                               total_price=total_price,
+                                               standard_number=int(
+                                                   standard_number),
+                                               deluxe_number=int(
+                                                   deluxe_number),
+                                               premium_number=int(
+                                                   premium_number),
+                                               suite_number=int(suite_number))
         hotel_booking_instance.save()
-        
 
-        summary_data = {'hotel': hotel_instance, 'hotel_check_in': hotel_check_in, 'hotel_check_out': hotel_check_out,
+        summary_data = {'number_of_rooms': number_of_rooms, 'hotel': hotel_instance, 'hotel_check_in': hotel_check_in_formatted, 'hotel_check_out': hotel_check_out_formatted,
                         'days': days.days, 'price_per_night': price_per_night, 'total_price': total_price}
         return render(request, "hotel_final_summary.html", summary_data)
